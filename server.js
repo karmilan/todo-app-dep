f599c6760a72aad2adb13ac28aaffdb79866ad76
karmilan <65656196+karmilan@users.noreply.github.com>
2024-06-17 15:45:47 +0530

    port 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 8000;
+const port = 5000;
 
 // Middleware
 app.use(bodyParser.json());
