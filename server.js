c17e63e2728dd63900ac105168d3cc03fad56288
karmilan <65656196+karmilan@users.noreply.github.com>
2024-06-17 16:06:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = 5000;
+const port = 8000;
 
 // Middleware
 app.use(bodyParser.json());
